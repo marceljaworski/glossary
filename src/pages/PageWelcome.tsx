9d43b8db7d2cd2e4620b1d9bcc150cf112179cf2
Marcelearn <jaworskimarcel@gmail.com>
2022-12-15 16:25:23 +0100

    pages welkome

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,7 +1,21 @@
+import data from '../data/data.json'
+
 export const PageWelcome = () => {
+	
 	return (
 		<>
 			<p>Welcome to this site.</p>
+			<div className= "glossaryContainer">
+				{data.glossary.map( (el) =>
+				<div className= "glossaryCard" key={el.id}>
+					<h3>{el.id}. {el.term}</h3>
+					<p>{el.meaning}</p>
+				</div>  
+				
+				)}
+			</div>
+
+
 		</>
 	);
 };
