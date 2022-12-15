1b8c4989e6be544f9194a2fdb78825acabec3d06
Marcelearn <jaworskimarcel@gmail.com>
2022-12-15 22:49:36 +0100

    isOpen

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,13 +1,30 @@
+import { useEffect, useState } from 'react';
 import data from '../data/data.json'
 import Card from '../components/Card'
 
+interface ITerm {
+	term: string;
+	meaning: string;
+}
 export const PageWelcome = () => {
+	const [terms, setTerms] = useState<ITerm[]>([])
 	
+	useEffect(() => {
+		const _terms: ITerm[] = [];
+        data.glossary.forEach((rawTerm: any) => {
+            const _term: ITerm = {
+                ...rawTerm,
+                isOpen: false,
+            };
+            _terms.push(_term);
+        });
+		setTerms(_terms);
+	}, [])
 	return (
 		<>
-			<p>Welcome to this site.</p>
+			<p>There are {terms.length}</p>
 			<div className= "glossaryContainer">
-				{data.glossary.map( (el:any) =>
+				{terms.map( (el:any) =>
 				<Card el={el} key={el.id}/>
 				)}
 			</div>
